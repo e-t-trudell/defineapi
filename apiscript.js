271909b407b504a11f83268152c0de2d7f57afb5
Eric <ettrudell17@gmail.com>
2022-07-29 15:58:43 -0600

    second commit

--- apiscript.js
+++ apiscript.js
@@ -1,11 +1,17 @@
 console.log("Something tree related...")
 function tree(element){
-    $.get("https://api.dictionaryapi.dev/api/v2/entries/en/tree", function(data){
-        console.log(data[0].meanings[1].definitions[2].definition);
-        $('.result').text(data[0].meanings[1].definitions[2].definition);
+    const name = document.querySelector(".search").value;
+
+    $.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${name}`, function(data){
+        console.log(data[0].meanings[0].definitions[0].definition);
+        $('.result').text(data[0].meanings[0].definitions[0].definition);
         }
     )
 }
+// tree();
+
+
+
 
 // .definitions[0].definition
 // .definitions[0].definition
